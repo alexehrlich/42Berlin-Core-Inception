72a200e6bf6f6af9f163fe2bc2baeecce1e01dfb
Alexander Ehrlich <aehrlich@c4c2c6.42berlin.de>
2024-02-28 08:30:40 +0100

    change

--- srcs/requirements/mariadb/tools/mariadb_setup.sh
+++ srcs/requirements/mariadb/tools/mariadb_setup.sh
@@ -1,28 +1,23 @@
 #!/bin/bash
 
-#start the (by Docker) installed SQL service
+# Start the MySQL service
 service mysql start
 
-# Setup the data base
-# -e: Execute a single line in the command line
-
-#create a new database with the name from the .env
+# Setup the database
+# Create a new database with the name from the .env
 mysql -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;"
 
-#create a new user
+# Create a new user
 mysql -e "CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'localhost' IDENTIFIED BY '${SQL_PSWD}';"
 
-#give all privileages to the user
+# Give all privileges to the user
 mysql -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PSWD}';"
 
-#change the root user pwd to the one from the .env
+# Change the root user password to the one from the .env
 mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PSWD}';"
 
-#make the changes be effective immediately
+# Make the changes be effective immediately
 mysql -e "FLUSH PRIVILEGES;"
 
-#shutdown to make the changes happen
-mysqladmin -u root -p$SQL_ROOT_PSWD shutdown
-
-#start the mysql service again
-exec mysqld_safe
+# Restart the MySQL service
+service mysql restart
